43fd3e93ded42f3a0b060c9a0a298e83a1bf95c4
Martin <martin@tmtron.com>
2017-05-18 13:09:41 +0200

    initial version

---
+++ app/src/main/java/com/tmtron/greenannotationstest/MainActivity.java
@@ -0,0 +1,43 @@
+package com.tmtron.greenannotationstest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tmtron.greenannotations.EventBusGreenRobot;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @ViewById(R.id.tvMain) TextView tvMain;
+
+    @EventBusGreenRobot
+    EventBus eventBus;
+
+    @Override
+    public void onStart() {
+        Toast.makeText(getApplicationContext(), "Before Start", Toast.LENGTH_SHORT).show();
+        super.onStart();
+    }
+
+
+    @Click
+    void btnTest() {
+        eventBus.post(new MessageEvent("+."));
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+        Toast.makeText(getApplicationContext(), event.getMessage(), Toast.LENGTH_SHORT).show();
+        tvMain.setText(tvMain.getText()+"-.");
+    }
+
+}
---
+++ app/src/main/java/com/tmtron/greenannotationstest/MessageEvent.java
@@ -0,0 +1,14 @@
+package com.tmtron.greenannotationstest;
+
+public class MessageEvent {
+
+    private final String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
